d7ed570cc0921f6e1ac21520b1e34a66f5f31bc7
glauglaucio <trabalhoglaucio@gmail.com>
2022-10-10 23:49:55 -0300

    dado

--- DadosRPG/dados.py
+++ DadosRPG/dados.py
@@ -1,3 +1,17 @@
+from numero import numeros1_100
 import os
 import random
 os.system("cls")
+print("Neste programa você irá poder rolar vários tipos de dados.\nApenas escolha qual dos tipos de dados abaixo você deseja rolar.\n\nD3, D4, D6, D8, D12, D20, D100.")
+
+while True:
+    dado = input("Caso deseje sair digite 0.\n\nCaso contrário digite o número do dado que deseja e o resto é comigo:")
+    os.system("cls")
+
+    if dado in ["3","4","6","8","12","20","100"]:
+        print("O resultado de sua rolada deu {}.\n".format(random.choice(numeros1_100[0:int(dado)])))
+    elif dado == "0":
+        break
+    else:
+        print("Não temos esse dado. :(\n")
+    print("Quer rolar mais algum dado?")
---
+++ DadosRPG/numero.py
@@ -0,0 +1,5 @@
+numeros1_100=[]
+i = 1
+while  i < 101:
+    numeros1_100.append(i)
+    i+=1
