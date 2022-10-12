65c332df11c281fd9d65926bf7f123d3322a462c
glauglaucio <trabalhoglaucio@gmail.com>
2022-10-11 22:30:49 -0300

    forca

--- Forca/forca.py
+++ Forca/forca.py
@@ -1,47 +1,68 @@
-from operator import le
 from bonequinhos import stages
 from palavras import palavras
 import os
 import random
-os.system("cls")
-i = m = 0
-t = 6
-tentativas = []
-palavMent = []
-palavraCerta = random.choice(palavras)
-palavC = list(palavraCerta)
-print("Bem vindo ao jogo da Forca mais dificil do mundo.\nApenas escolha uma letra e eu te digo se acertou ou não.")
 
-while m < len(palavraCerta):
-    palavMent.insert(m,"_")
-    m+=1
+def jogarDnv():
+    while True:
+        print("\n\nVai querer jogar de novo meu amigo?")
+        resp = input("Digite \"s\" para continuar ou \"n\" para sair:").lower()
+        if resp == "s":
 
-while t > 0:
-    print("Voce tem",t,"tentativas agora... cuidado.")
-    print(stages[t])
-    print(' '.join(palavMent))
-    letra = input("\nFaça sua escolha:").lower()
-    os.system("cls")
-    
-    if letra in tentativas:
-        print("\nChutando a mesma letra? aaa para ne.\n")
-    elif letra not in palavC:
-        t-=1
-        if t == 0:
-            print(stages[0],"\nVixe amigo, perdeu ein.\nTudo bem, voce pode tentar de novo na próxima.\nPor via das dúvidas a palavra era",palavraCerta,end = ".")
+            break
+        elif resp == "n":
             exit()
-        print("\nHmm... sei não ein, acho que essa letra ai não tem.\n")  
-    elif letra in palavC:
-        while i < len(palavraCerta):
-            if letra in palavC[i]:
-                palavMent[i]=letra
-                if "_" not in palavMent:
-                    t = 0
-                    print(stages[6],"\nOQUE? COMO?\nCOMO VOCE GANHOU?\nOk, só conseguiu ganhar pq tava fácil.")
-                    exit()
-                i+=1
+        else:
+            os.system("cls")
+            print("Desculpe, não entendi.")
+    global a
+    a = 1
+
+while True:
+    os.system("cls")
+    print("Bem vindo ao jogo da Forca mais dificil do mundo.\nApenas escolha uma letra e eu te digo se acertou ou não.")
+    i = m = a = 0
+    t = 6
+    tentativas = []
+    palavMent = []
+    palavraCerta = random.choice(palavras)
+    palavC = list(palavraCerta)
+    
+    while m < len(palavraCerta):
+        palavMent.insert(m,"_")
+        m+=1
+
+
+    while t > 0:
+        print("Voce tem",t,"tentativas agora... cuidado.")
+        print(stages[t])
+        print(' '.join(palavMent))
+        sera = letra = input("\nFaça sua escolha:").lower()
+        os.system("cls")
+
+        if letra == "sair":
+            exit()
+        if letra in tentativas:
+            print("\nChutando a mesma letra? aaa para ne.\n")
+        elif letra not in palavC:
+            t-=1
+            if t == 0:
+                print(stages[0],"\nVixe amigo, perdeu ein.\nTudo bem, voce pode tentar de novo na próxima.\nPor via das dúvidas a palavra era",palavraCerta,end = ".")
+                jogarDnv()
             else:
+                print("\nHmm... sei não ein, acho que essa letra ai não tem.\n")  
+        elif letra in palavC:
+            while i < len(palavraCerta):
+
+                if letra in palavC[i]:
+                    palavMent[i]=letra
                 i+=1
-        print("\nIMPOSSIVEL, COMO VOCE SABE?\n")
-        i=0  
-    tentativas.extend(letra)
+            print("\nIMPOSSIVEL, COMO VOCE SABE?\n")
+            i=0
+        if "_" not in palavMent or sera == palavraCerta:
+            t = 0
+            print(stages[6],"\nOQUE? COMO?\nCOMO VOCE GANHOU?\nOk, só conseguiu ganhar pq tava fácil.")
+            jogarDnv()
+        if a == 1:
+            break
+        tentativas.append(letra)
